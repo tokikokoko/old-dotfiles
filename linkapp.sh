8aff135e9e29a54cfed19722c176201abfbb309e
tokiko <toki.linuxx@live.jp>
2017-02-15 05:11:31 +0900

    [add]make soume symbolic link when init env.

---
+++ linkapp.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+ln -s $SCRIPT_DIR/.emacs.d $HOME
+ln -s $SCRIPT_DIR/.vimrc $HOME
+ln -s $SCRIPT_DIR/.gvimrc $HOME
+ln -s $SCRIPT_DIR/.tmux.conf $HOME
+ln -s $SCRIPT_DIR/.vim $HOME
