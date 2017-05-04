86ee29fb346c7c484b5fae3b1ec31c9616f2016f
tokikokoko <toki.linuxx@live.jp>
2017-05-04 22:30:53 +0900

    [fix].zshrcの追加

--- linkapp.sh
+++ linkapp.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+ln -s $SCRIPT_DIR/.zshrc $HOME
 ln -s $SCRIPT_DIR/.emacs.d $HOME
 ln -s $SCRIPT_DIR/.vimrc $HOME
 ln -s $SCRIPT_DIR/.gvimrc $HOME
