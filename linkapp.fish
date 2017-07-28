53e870d1d2af8888e8b855c6058ecf513aadccde
tokiko <toki.linuxx@live.jp>
2017-07-28 13:36:17 +0900

    [delete]使用しないため削除.

--- linkapp.fish
+++
@@ -1,9 +0,0 @@
-cd (dirname (status -f))
-echo $PWD
-rm -rf $HOME/.config/fish
-ln -s $PWD/.config/fish $HOME/.config
-ln -s $PWD/.emacs.d $HOME
-ln -s $PWD/.vimrc $HOME
-ln -s $PWD/.gvimrc $HOME
-ln -s $PWD/.tmux.conf $HOME
-ln -s $PWD/.vim $HOME
