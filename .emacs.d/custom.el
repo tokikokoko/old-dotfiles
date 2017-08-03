31ca02ca98b0f8c0e07485bf437296209c2321c5
Keita Iwatani <toki.linuxx@live.jp>
2017-08-03 15:31:29 +0900

    [new]Install magit.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (web-mode elpy zenburn-theme go-mode flycheck indent-guide moe-theme slime counsel ivy-rich swiper company fish-mode use-package package-utils))))
+    (magit web-mode elpy zenburn-theme go-mode flycheck indent-guide moe-theme slime counsel ivy-rich swiper company fish-mode use-package package-utils))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
