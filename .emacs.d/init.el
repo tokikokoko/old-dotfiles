bad1f94fe6990a737b898769f7366eab17dbd342
tokiko <toki.linuxx@live.jp>
2017-08-14 15:48:02 +0900

    [fix]インストール出来ない不具合修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,7 @@
 ;;-> 00.Package Settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (package-initialize)
+(package-refresh-contents)
 ;; リポジトリの設定
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -64,6 +65,7 @@
 (defun package-install-all ()
   (interactive)
   (package-refresh-contents)
+  ;(package-refresh-contents)
   (dolist (package my/favorite-packages)
   (unless (package-installed-p package)
     (package-install package)))
