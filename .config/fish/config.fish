99aebb5939a92f1bf8d2f0300d465b8906834f8f
tokiko <toki.linuxx@live.jp>
2017-08-19 03:23:19 +0900

    [fix]環境変数の設定等々

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,21 @@
 # fish config
 # ############
 # alias & function 読み込み
-. ~/.config/fish/aliases.fish
+. $HOME/.config/fish/aliases.fish
 
-set -x PATH $PATH
+# 環境変数読み込み
+if status --is-login
+	. $HOME/.config/fish/env.fish
+end
+
+# 環境依存設定読み込み
+if test -f $HOME/local.fish
+	. $HOME/local.fish	
+end
 
 # Ensure fisherman and plugins are installed
 if not test -f $HOME/.config/fish/functions/fisher.fish
-  echo "==> Fisherman not found.  Installing."
-  curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
-  fisher
+	echo "==> Fisherman not found.  Installing."
+	curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+	fisher
 end
---
+++ .config/fish/env.fish
@@ -0,0 +1,14 @@
+# PATH
+
+set -x PATH $HOME/.nodebrew/current/bin $PATH
+set -x PATH /usr/local/go/bin $PATH
+set -x PATH $HOME/.go/bin $PATH
+
+# general
+set DOT $HOME/dotfiles
+set -x TERM xterm-256color
+set -x EDITOR emacs
+
+# golang
+set -x GOPATH $HOME/.go
+
