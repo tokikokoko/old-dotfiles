ab984e3ac1c99e600458eab21bd23a12d8aa070a
tokiko <toki.linuxx@live.jp>
2017-08-19 03:48:26 +0900

    [fix]promptの修正

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -3,6 +3,17 @@ alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
 
+# prompt
+function fish_prompt
+	test $SSH_TTY
+    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+    test $USER = 'root'
+    and echo (set_color red)"#"
+
+    # Main
+    echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
+
 # vim
 alias vim='nvim'
 
