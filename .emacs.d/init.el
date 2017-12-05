4276325704608be0ec9102de6ae0956e6f0b5591
tokiko <toki.linuxx@live.jp>
2017-12-06 01:59:00 +0900

    Fix complement length

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -257,7 +257,7 @@
   :config
   (global-company-mode)
   (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 0)
+  (setq company-minimum-prefix-length 2)
   (setq company-selection-wrap-around t)
   ;; keys
   (define-key company-active-map (kbd "M-n") nil)
