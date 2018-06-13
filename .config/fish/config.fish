d54543e321d510bc2808d4ee1d4343202c9bf91b
Keita Iwatani <iwatani_keita@uniquevision.co.jp>
2018-06-13 12:10:09 +0900

    Fix indent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@
 
 # 環境変数読み込み
 if status --is-login
-	. $HOME/.config/fish/env.fish
+    . $HOME/.config/fish/env.fish
 end
 
 # function & alias読み込み
@@ -13,12 +13,12 @@ end
 
 # 環境依存設定読み込み
 if test -f $HOME/local.fish
-	. $HOME/local.fish	
+    . $HOME/local.fish	
 end
 
 # Ensure fisherman and plugins are installed
 if not test -f $HOME/.config/fish/functions/fisher.fish
-	echo "==> Fisherman not found.  Installing."
-	curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
-	fisher
+    echo "==> Fisherman not found.  Installing."
+    curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+    fisher
 end
--- .config/fish/user_functions.fish
+++ .config/fish/user_functions.fish
@@ -11,17 +11,17 @@
 
 # emacs-daemon
 function restart_server -d "Restart emacsclient"
-	emacsclient -e '(kill-emacs)'
-	emacs --daemon
+    emacsclient -e '(kill-emacs)'
+    emacs --daemon
 end
 
 function kill_server -d "Stop emacsclient"
-	emacsclient -e '(kill-emacs)'
+    emacsclient -e '(kill-emacs)'
 end
 
 # shell
 function relogin -d "Re-login current shell"
-	exec /usr/local/bin/fish -l
+    exec /usr/local/bin/fish -l
 end
 
 # auto execute ctags
@@ -30,37 +30,37 @@ end
 # Result
 # -> execute ctags every 300sec
 function AutoCtagsP
-	set rem (math '$argv[1] % 10')
-	set div (math '$argv[1] / 10')
+    set rem (math '$argv[1] % 10')
+    set div (math '$argv[1] / 10')
+    while true
+	set count 1
+	set flag false
+	ctags -R . $argv[2]
 	while true
-		set count 1
-		set flag false
-		ctags -R . $argv[2]
-		while true
-			if math '$count > $div'
-				break
-			end
-			sleep 10
-			echo (math '$count * 10')
-			set count (math '$count + 1')
-		end
-		sleep $rem
+	    if math '$count > $div'
+		break
+	    end
+	    sleep 10
+	    echo (math '$count * 10')
+	    set count (math '$count + 1')
 	end
+	sleep $rem
+    end
 end
 
 function AutoCtags
-	while true
-		ctags -R . $argv[2]
-		sleep $argv[1]
-	end
+    while true
+	ctags -R . $argv[2]
+	sleep $argv[1]
+    end
 end
 
 # git get branches
 function get_git_branches
-	git branch --list --all | awk '{print substr($0, 3, length($0))}'
+    git branch --list --all | awk '{print substr($0, 3, length($0))}'
 end
 
 # git checkout
 function gc
-	git checkout (get_git_branches | fzf)
+    git checkout (get_git_branches | fzf)
 end
