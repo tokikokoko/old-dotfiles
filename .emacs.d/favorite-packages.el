8c1fb5595e0030c554e785f223d2f69e0dca87f2
Keita Iwatani <iwatani_keita@uniquevision.co.jp>
2019-01-23 14:36:31 +0900

    Divide unuse function
    * installer.sh:

---
+++ .emacs.d/favorite-packages.el
@@ -0,0 +1,43 @@
+;;; Code: packagelists
+(defvar my/favorite-packages
+  '(
+    ;; package-utils
+    package-utils
+    ;; theme
+    zenburn-theme dracula-theme spacemacs-theme
+    color-theme-sanityinc-tomorrow
+    ;; general
+    multiple-cursors indent-guide shackle
+    projectile rainbow-delimiters ripgrep helm-ag
+    spaceline linum-relative editorconfig hydra
+    visual-regexp elscreen 
+    ;; git
+    magit
+    ;; company
+    company
+    ;; ivy-mode
+    swiper ivy-rich counsel
+    counsel-projectile
+    ;; org
+    ox-pandoc org-bullets
+    ;; flycheck
+    flycheck
+    ;; fish-shell-mode
+    fish-mode
+    ;; docker-file
+    dockerfile-mode
+    ;; elixir
+    elixir-mode alchemist flycheck-elixir
+    ;; go
+    go-mode
+    ;; ruby
+    ruby-electric
+    ;; python
+    python-mode jedi
+    ;; lisp
+    slime
+    ;; markdown
+    mkdown
+    ;; web
+    web-mode
+    ))
